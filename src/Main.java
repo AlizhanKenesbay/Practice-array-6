1c2af319f25e15bd60d34440f349c96b14d363e8
Alizhan Kenesbay <alzhnk@gmail.com>
2021-10-09 18:00:30 +0600

    Practice

--- src/Main.java
+++ src/Main.java
@@ -3,19 +3,24 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         int[] array = new int[4];
-        int checkIndex = array[0];
+        boolean flag = true;
 
         for (int i = 0; i < array.length; i++) {
             array[i] = (int) (Math.random() * 90) + 10;
         }
         System.out.println(Arrays.toString(array));
 
-        for (int i = 0; i < array.length; i++) {
-            if (array[i] > checkIndex) {
-                System.out.println("Массив возрастающий");
-            } else {
-                System.out.println("Массив не возрастающий");
+        for (int i = 1; i < array.length; i++) {
+            if(array[i] <= array[i-1]){
+                flag = false;
+                break;
             }
+        }
+
+        if(flag){
+            System.out.println("Массив является возрастающей последовательно");
+        } else {
+            System.out.println("Массив не является возрастающим последовательно");
         }
     }
 }
